9676851e898f44a5305395f0128b1f3082c501f2
FORSAIN <jeanluc.forsain@gmail.com>
2016-03-20 21:38:40 +0100

    Modification Piece.java
    Signed-off-by: FORSAIN <jeanluc.forsain@gmail.com>

--- src/main/java/modele/Piece.java
+++ src/main/java/modele/Piece.java
@@ -3,4 +3,33 @@ package modele;
 public class Piece {
 	
 	
+	public void pivoterGauche()
+	{
+		
+	}
+	
+	public void pivoterDroit()
+	{
+		
+	}
+	
+	public void deplacerGauche()
+	{
+		
+	}
+	
+	public void deplacerDroite()
+	{
+		
+	}
+	
+	public void descenteInstantanee()
+	{
+		
+	}
+	
+	public void desecenteAcceleree()
+	{
+		
+	}
 }
